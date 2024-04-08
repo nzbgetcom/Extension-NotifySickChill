72c51f2fefb6f27fc4c7f2f3356fd1b74dceb185
dnzbk <denis@nzbget.com>
2024-04-08 15:25:45 +0300

    init

---
+++ main.py
@@ -0,0 +1,145 @@
+#
+# This file is part of nzbget. See <https://nzbget.com>.
+#
+# Copyright (C) 2024 Denis <denis@nzbget.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+import os
+import sys
+import requests
+from json import JSONDecodeError
+
+SUCCESS = 93
+ERROR = 94
+NONE = 95
+
+METHODS_MAP = {
+	'Copy': 'copy',
+	'Move': 'move',
+	'Hard Link': 'hardlink',
+	'Symbolic Link': 'symlink',
+}
+
+REQUIRED_OPTIONS = [
+	'NZBPP_DIRECTORY',
+	'NZBPO_APIKEY',
+	'NZBPO_HOST',
+	'NZBPO_PORT',
+	'NZBPO_PROCESSMETHOD',
+	'NZBPO_FORCEREPLACE',
+	'NZBPO_ISPRIORITY',
+	'NZBPO_VERBOSE',
+]
+
+def validate_options(options: list) -> None:
+	for	optname in options:
+		if (not optname in os.environ):
+			print(f'[ERROR] Option {optname[6:]} is missing in configuration file. Please check extension settings.')
+			return ERROR
+
+	method = os.environ['NZBPO_PROCESSMETHOD']
+	if METHODS_MAP.get(method, '') == '':
+		return ERROR
+
+	return SUCCESS
+
+if validate_options(REQUIRED_OPTIONS) == ERROR:
+	sys.exit(ERROR)
+
+
+API_KEY = os.environ['NZBPO_APIKEY']
+HOST = os.environ['NZBPO_HOST']
+PORT = os.environ['NZBPO_PORT']
+PATH = os.environ.get('NZBPP_FINALDIR') or os.environ.get('NZBPP_DIRECTORY', '')
+PROCESS_METHOD = METHODS_MAP.get[os.environ['NZBPO_PROCESSMETHOD']]
+FORCE_REPLACE = int(os.environ['NZBPO_FORCEREPLACE'] == 'yes')
+IS_PRIORITY = int(os.environ['NZBPO_ISPRIORITY'] == 'yes')
+VERBOSE = os.environ['NZBPO_VERBOSE'] == 'yes'
+COMMAND = os.environ.get('NZBCP_COMMAND', '') == 'ping'
+
+URL = f'http://{HOST}:{PORT}/api/{API_KEY}'
+
+if VERBOSE: 
+	print('[INFO] URL:', URL)
+	print('[INFO] PATH:', PATH)
+
+def ping_sickchill(url: str) -> int:
+	params = { 'cmd': 'sb.ping' }
+
+	if VERBOSE:
+		print('[INFO] PARAMS:', params)
+
+	try:
+		response = requests.get(url, params=params).json()
+
+		if response['result'] == 'success':
+			print('[INFO] SickChill pinged successfully:', response['message'])
+			return SUCCESS
+
+		print('[ERROR] Couldn\'t ping:', response['message'])
+		return ERROR
+	
+	except JSONDecodeError as ex:
+		print('[ERROR] Wrong API Key?')
+		return ERROR
+
+	except Exception as ex:
+		print('[ERROR] Unexpected exception:', ex)
+		return ERROR
+	
+def start_post_proccessing(url: str, path: str, process_method: str, force_replace: int, is_priority: int) -> int:
+	params = {
+		'cmd': 'postprocess',
+		'path': path,
+		'process_method': process_method,
+		'force_replace': force_replace,
+		'is_priority': is_priority,
+	}
+
+	if VERBOSE:
+		print(f'[INFO] PARAMS:', params)
+
+	try:
+		response = requests.get(url, params=params).json()
+
+		if response['result'] == 'success':
+			print('[INFO] Post-proccessing started successfully:', response['message'])
+			return SUCCESS
+
+		print('[ERROR] Couldn\'t start Post-proccessing:', response['message'])
+		return ERROR
+
+	except JSONDecodeError as ex:
+		print('[ERROR] Wrong API Key?')
+
+	except Exception as ex:
+		print('[ERROR] Unexpected exception:', ex)
+		return ERROR
+
+
+if COMMAND:
+	sys.exit(ping_sickchill(URL))
+
+sys.exit(
+	start_post_proccessing(
+		URL, 
+		PATH, 
+		PROCESS_METHOD, 
+		FORCE_REPLACE,
+		IS_PRIORITY,
+	)
+)
---
+++ tests.py
@@ -0,0 +1,136 @@
+#
+# This file is part of nzbget. See <https://nzbget.com>.
+#
+# Copyright (C) 2024 Denis <denis@nzbget.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+import sys
+from os.path import dirname
+import os
+import subprocess
+import http.server
+import threading
+import unittest
+import json
+from urllib.parse import urlparse, parse_qs
+
+SUCCESS=93
+NONE=95
+ERROR=94
+
+ROOT_DIR = dirname(__file__)
+HOST = '127.0.0.1'
+PORT = '6789'
+
+class HttpServerPingMock(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		
+		data = {'data': {'pid': 5124}, 'message': 'Pong', 'result': 'success'}
+		response = json.dumps(data)
+		self.wfile.write(response.encode('utf-8'))
+
+class HttpServerPostprocMock(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		parsed_url = urlparse(self.path)
+		query_params = parse_qs(parsed_url.query)
+		cmd = query_params.get('cmd', [''])[0] == 'postprocess'
+		path = query_params.get('path', [''])[0] == ROOT_DIR
+		process_method = query_params.get('process_method', [''])[0] == 'move'
+		force_replace = query_params.get('force_replace', [''])[0] == '1'
+		is_priority = query_params.get('is_priority', [''])[0] == '1'
+
+		if (cmd and path and process_method and force_replace and is_priority):
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			data = {'data': {}, 'message': 'Started', 'result': 'success'}
+			response = json.dumps(data)
+			self.wfile.write(response.encode('utf-8'))
+		else:
+			self.send_response(400)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			
+			data = {'data': {}, 'message': 'Failure', 'result': 'failure'}
+			response = json.dumps(data)
+			self.wfile.write(response.encode('utf-8'))
+
+def get_python(): 
+	if os.name == 'nt':
+		return 'python'
+	return 'python3'
+
+def run_script():
+	sys.stdout.flush()
+	proc = subprocess.Popen([get_python(), ROOT_DIR + '/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy())
+	out, err = proc.communicate()
+	ret_code = proc.returncode
+	return (out.decode(), int(ret_code), err.decode())
+
+def set_default_env():
+	os.environ['NZBPO_APIKEY'] = 'API_KEY'
+	os.environ['NZBPP_DIRECTORY'] = ROOT_DIR
+	os.environ['NZBPO_HOST'] = HOST
+	os.environ['NZBPO_PORT'] = PORT
+	os.environ['NZBPO_PROCESSMETHOD'] = 'Copy'
+	os.environ['NZBPO_FORCEREPLACE'] = 'yes'
+	os.environ['NZBPO_ISPRIORITY'] = 'yes'
+	os.environ['NZBPO_VERBOSE'] = 'no'
+	os.environ['NZBCP_COMMAND'] = ''
+
+class Tests(unittest.TestCase):
+	def test_command(self):
+		set_default_env()
+		os.environ['NZBCP_COMMAND'] = 'ping'
+		server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPingMock)
+		thread = threading.Thread(target=server.serve_forever)
+		thread.start()
+		[_, code, _] = run_script()
+		server.shutdown()
+		server.server_close()
+		thread.join()
+		self.assertEqual(code, SUCCESS)
+
+	def test_postproc(self):
+		set_default_env()
+		os.environ['NZBPO_PROCESSMETHOD'] = 'Move'
+		server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
+		thread = threading.Thread(target=server.serve_forever)
+		thread.start()
+		[_, code, _] = run_script()
+		server.shutdown()
+		server.server_close()
+		thread.join()
+		self.assertEqual(code, SUCCESS)
+	
+	def test_wrong_postproc_method(self):
+		set_default_env()
+		os.environ['NZBPO_PROCESSMETHOD'] = 'Wrong method'
+		server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
+		thread = threading.Thread(target=server.serve_forever)
+		thread.start()
+		[_, code, _] = run_script()
+		server.shutdown()
+		server.server_close()
+		thread.join()
+		self.assertEqual(code, ERROR)
+
+if __name__ == '__main__':
+	unittest.main()
