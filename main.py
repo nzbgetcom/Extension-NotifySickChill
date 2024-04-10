8853e4af8b43c0ebc900fd0a7963f9314c9c1360
dnzbk <denis@nzbget.com>
2024-04-10 12:38:07 +0300

    Use spaces, fix pep8 warnings

--- main.py
+++ main.py
@@ -30,36 +30,37 @@ ERROR = 94
 NONE = 95
 
 METHODS_MAP = {
-	'Copy': 'copy',
-	'Move': 'move',
-	'Hard Link': 'hardlink',
-	'Symbolic Link': 'symlink',
+    'Copy': 'copy',
+    'Move': 'move',
+    'Hard Link': 'hardlink',
+    'Symbolic Link': 'symlink',
 }
 
 REQUIRED_OPTIONS = [
-	'NZBPO_APIKEY',
-	'NZBPO_HOST',
-	'NZBPO_PORT',
-	'NZBPO_PROCESSMETHOD',
-	'NZBPO_FORCEREPLACE',
-	'NZBPO_ISPRIORITY',
-	'NZBPO_VERBOSE',
+    'NZBPO_APIKEY',
+    'NZBPO_HOST',
+    'NZBPO_PORT',
+    'NZBPO_PROCESSMETHOD',
+    'NZBPO_FORCEREPLACE',
+    'NZBPO_ISPRIORITY',
+    'NZBPO_VERBOSE',
 ]
 
-def validate_options(options: list, methods_map: dict) -> None:
-	for	optname in options:
-		if (not optname in os.environ):
-			print(f'[ERROR] Option {optname[6:]} is missing in configuration file. Please check extension settings.')
-			sys.exit(ERROR)
-	
-	method = os.environ.get('NZBPO_PROCESSMETHOD')
-	if method == None or methods_map.get(method) == None:
-		print(f'[ERROR] Unsupported process method: {method}.')
-		sys.exit(ERROR)
 
-	if not os.environ.get('NZBCP_COMMAND') and not os.environ.get('NZBPP_DIRECTORY') and not os.environ.get('NZBPP_FINALDIR'):
-		print(f'[ERROR] The path to the downloaded file is not provided.')
-		sys.exit(ERROR)
+def validate_options(options: list, methods_map: dict) -> None:
+    for optname in options:
+        if optname not in os.environ:
+            print(f'[ERROR] Option {optname[6:]} is missing in configuration file. Please check extension settings.')
+            sys.exit(ERROR)
+
+    method = os.environ.get('NZBPO_PROCESSMETHOD')
+    if method is None or methods_map.get(method) is None:
+        print(f'[ERROR] Unsupported process method: {method}.')
+        sys.exit(ERROR)
+
+    if not os.environ.get('NZBCP_COMMAND') and not os.environ.get('NZBPP_DIRECTORY') and not os.environ.get('NZBPP_FINALDIR'):
+        print(f'[ERROR] The path to the downloaded file is not provided.')
+        sys.exit(ERROR)
 
 
 validate_options(REQUIRED_OPTIONS, METHODS_MAP)
@@ -74,71 +75,78 @@ COMMAND = os.environ.get('NZBCP_COMMAND') == 'ping'
 
 URL = f'http://{HOST}:{PORT}/api/{API_KEY}'
 
-if VERBOSE: 
-	print('[INFO] URL:', URL)
+
+if VERBOSE:
+    print('[INFO] URL:', URL)
+
 
 def ping_sickchill(url: str) -> int:
-	try:
-		encoded_params = urllib.parse.urlencode({ 'cmd': 'sb.ping' })
+    try:
+        encoded_params = urllib.parse.urlencode({'cmd': 'sb.ping'})
 
-		if VERBOSE:
-			print(f'[INFO] PARAMS:', encoded_params)
+        if VERBOSE:
+            print(f'[INFO] PARAMS:', encoded_params)
 
-		full_url = f'{url}?{encoded_params}'
-		with urllib.request.urlopen(full_url) as response:
-			data = response.read().decode('utf-8')
-			response_dict = json.loads(data)
+        full_url = f'{url}?{encoded_params}'
+        with urllib.request.urlopen(full_url) as response:
+            data = response.read().decode('utf-8')
+            response_dict = json.loads(data)
 
-			if response_dict['result'] == 'success':
-				print('[INFO] SickChill pinged successfully:', response_dict['message'])
-				return SUCCESS
+            if response_dict['result'] == 'success':
+                print('[INFO] SickChill pinged successfully:', response_dict['message'])
+                return SUCCESS
 
-			print('[ERROR] Couldn\'t ping:', response_dict['message'])
-			return ERROR
-	
-	except JSONDecodeError as ex:
-		print('[ERROR] Wrong API Key?')
-		return ERROR
+            print('[ERROR] Couldn\'t ping:', response_dict['message'])
+            return ERROR
 
-	except Exception as ex:
-		print('[ERROR] Unexpected exception:', ex)
-		return ERROR
-	
-def start_post_proccessing(url: str, path: str, process_method: str, force_replace: int, is_priority: int) -> int:
-	try:
-		encoded_params = urllib.parse.urlencode({
-			'cmd': 'postprocess',
-			'path': path,
-			'process_method': process_method,
-			'force_replace': force_replace,
-			'is_priority': is_priority,
-		})
+    except JSONDecodeError as ex:
+        print('[ERROR] Wrong API Key?')
+        return ERROR
 
-		if VERBOSE:
-			print(f'[INFO] PARAMS:', encoded_params)
+    except Exception as ex:
+        print('[ERROR] Unexpected exception:', ex)
+        return ERROR
 
-		full_url = f'{url}?{encoded_params}'
-		with urllib.request.urlopen(full_url) as response:
-			data = response.read().decode('utf-8')
-			response_dict = json.loads(data)
 
-			if response_dict['result'] == 'success':
-				print('[INFO] Post-processing started successfully:', response_dict['message'])
-				return SUCCESS
+def start_post_proccessing(url: str,
+                           path: str,
+                           process_method: str,
+                           force_replace: int,
+                           is_priority: int) -> int:
+    try:
+        encoded_params = urllib.parse.urlencode({
+            'cmd': 'postprocess',
+            'path': path,
+            'process_method': process_method,
+            'force_replace': force_replace,
+            'is_priority': is_priority,
+        })
 
-			print('[ERROR] Couldn\'t start Post-processing:', response_dict['message'])
-			return ERROR
+        if VERBOSE:
+            print(f'[INFO] PARAMS:', encoded_params)
 
-	except JSONDecodeError as ex:
-		print('[ERROR] Wrong API Key?')
+        full_url = f'{url}?{encoded_params}'
+        with urllib.request.urlopen(full_url) as response:
+            data = response.read().decode('utf-8')
+            response_dict = json.loads(data)
 
-	except Exception as ex:
-		print('[ERROR] Unexpected exception:', ex)
-		return ERROR
+            if response_dict['result'] == 'success':
+                print('[INFO] Post-processing started successfully:', response_dict['message'])
+                return SUCCESS
+
+            print('[ERROR] Couldn\'t start Post-processing:', response_dict['message'])
+            return ERROR
+
+    except JSONDecodeError as ex:
+        print('[ERROR] Wrong API Key?')
+
+    except Exception as ex:
+        print('[ERROR] Unexpected exception:', ex)
+        return ERROR
 
 
 if COMMAND:
-	sys.exit(ping_sickchill(URL))
+    sys.exit(ping_sickchill(URL))
 
 
 PATH = os.environ.get('NZBPP_FINALDIR') or os.environ['NZBPP_DIRECTORY']
@@ -146,15 +154,17 @@ PROCESS_METHOD = METHODS_MAP[os.environ['NZBPO_PROCESSMETHOD']]
 FORCE_REPLACE = int(os.environ['NZBPO_FORCEREPLACE'] == 'yes')
 IS_PRIORITY = int(os.environ['NZBPO_ISPRIORITY'] == 'yes')
 
+
 if VERBOSE:
-	print('[INFO] PATH:', PATH)
+    print('[INFO] PATH:', PATH)
+
 
 sys.exit(
-	start_post_proccessing(
-		URL, 
-		PATH, 
-		PROCESS_METHOD, 
-		FORCE_REPLACE,
-		IS_PRIORITY,
-	)
+    start_post_proccessing(
+        URL,
+        PATH,
+        PROCESS_METHOD,
+        FORCE_REPLACE,
+        IS_PRIORITY,
+    )
 )
