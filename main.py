4f0c7534028266e248eb3e0c2fc5b8ac9fef7525
dnzbk <denis@nzbget.com>
2024-04-10 14:38:26 +0300

    linting

--- main.py
+++ main.py
@@ -30,118 +30,127 @@ ERROR = 94
 NONE = 95
 
 METHODS_MAP = {
-    'Copy': 'copy',
-    'Move': 'move',
-    'Hard Link': 'hardlink',
-    'Symbolic Link': 'symlink',
+    "Copy": "copy",
+    "Move": "move",
+    "Hard Link": "hardlink",
+    "Symbolic Link": "symlink",
 }
 
 REQUIRED_OPTIONS = [
-    'NZBPO_APIKEY',
-    'NZBPO_HOST',
-    'NZBPO_PORT',
-    'NZBPO_PROCESSMETHOD',
-    'NZBPO_FORCEREPLACE',
-    'NZBPO_ISPRIORITY',
-    'NZBPO_VERBOSE',
+    "NZBPO_APIKEY",
+    "NZBPO_HOST",
+    "NZBPO_PORT",
+    "NZBPO_PROCESSMETHOD",
+    "NZBPO_FORCEREPLACE",
+    "NZBPO_ISPRIORITY",
+    "NZBPO_VERBOSE",
 ]
 
 
 def validate_options(options: list, methods_map: dict) -> None:
     for optname in options:
         if optname not in os.environ:
-            print(f'[ERROR] Option {optname[6:]} is missing in configuration file. Please check extension settings.')
+            print(
+                f"[ERROR] Option {optname[6:]} is missing in configuration file. Please check extension settings."
+            )
             sys.exit(ERROR)
 
-    method = os.environ.get('NZBPO_PROCESSMETHOD')
+    method = os.environ.get("NZBPO_PROCESSMETHOD")
     if method is None or methods_map.get(method) is None:
-        print(f'[ERROR] Unsupported process method: {method}.')
+        print(f"[ERROR] Unsupported process method: {method}.")
         sys.exit(ERROR)
 
-    if not os.environ.get('NZBCP_COMMAND') and not os.environ.get('NZBPP_DIRECTORY') and not os.environ.get('NZBPP_FINALDIR'):
-        print(f'[ERROR] The path to the downloaded file is not provided.')
+    if (
+        not os.environ.get("NZBCP_COMMAND")
+        and not os.environ.get("NZBPP_DIRECTORY")
+        and not os.environ.get("NZBPP_FINALDIR")
+    ):
+        print(f"[ERROR] The path to the downloaded file is not provided.")
         sys.exit(ERROR)
 
 
 validate_options(REQUIRED_OPTIONS, METHODS_MAP)
 
 
-API_KEY = os.environ['NZBPO_APIKEY']
-HOST = os.environ['NZBPO_HOST']
-PORT = os.environ['NZBPO_PORT']
+API_KEY = os.environ["NZBPO_APIKEY"]
+HOST = os.environ["NZBPO_HOST"]
+PORT = os.environ["NZBPO_PORT"]
 
-VERBOSE = os.environ['NZBPO_VERBOSE'] == 'yes'
-COMMAND = os.environ.get('NZBCP_COMMAND') == 'ping'
+VERBOSE = os.environ["NZBPO_VERBOSE"] == "yes"
+COMMAND = os.environ.get("NZBCP_COMMAND") == "ping"
 
-URL = f'http://{HOST}:{PORT}/api/{API_KEY}'
+URL = f"http://{HOST}:{PORT}/api/{API_KEY}"
 
 
 if VERBOSE:
-    print('[INFO] URL:', URL)
+    print("[INFO] URL:", URL)
 
 
 def ping_sickchill(url: str) -> int:
     try:
-        encoded_params = urllib.parse.urlencode({'cmd': 'sb.ping'})
+        encoded_params = urllib.parse.urlencode({"cmd": "sb.ping"})
 
         if VERBOSE:
-            print(f'[INFO] PARAMS:', encoded_params)
+            print(f"[INFO] PARAMS:", encoded_params)
 
-        full_url = f'{url}?{encoded_params}'
+        full_url = f"{url}?{encoded_params}"
         with urllib.request.urlopen(full_url) as response:
-            data = response.read().decode('utf-8')
+            data = response.read().decode("utf-8")
             response_dict = json.loads(data)
 
-            if response_dict['result'] == 'success':
-                print('[INFO] SickChill pinged successfully:', response_dict['message'])
+            if response_dict["result"] == "success":
+                print("[INFO] SickChill pinged successfully:", response_dict["message"])
                 return SUCCESS
 
-            print('[ERROR] Couldn\'t ping:', response_dict['message'])
+            print("[ERROR] Couldn't ping:", response_dict["message"])
             return ERROR
 
     except JSONDecodeError as ex:
-        print('[ERROR] Wrong API Key?')
+        print("[ERROR] Wrong API Key?")
         return ERROR
 
     except Exception as ex:
-        print('[ERROR] Unexpected exception:', ex)
+        print("[ERROR] Unexpected exception:", ex)
         return ERROR
 
 
-def start_post_proccessing(url: str,
-                           path: str,
-                           process_method: str,
-                           force_replace: int,
-                           is_priority: int) -> int:
+def start_post_proccessing(
+    url: str, path: str, process_method: str, force_replace: int, is_priority: int
+) -> int:
     try:
-        encoded_params = urllib.parse.urlencode({
-            'cmd': 'postprocess',
-            'path': path,
-            'process_method': process_method,
-            'force_replace': force_replace,
-            'is_priority': is_priority,
-        })
+        encoded_params = urllib.parse.urlencode(
+            {
+                "cmd": "postprocess",
+                "path": path,
+                "process_method": process_method,
+                "force_replace": force_replace,
+                "is_priority": is_priority,
+            }
+        )
 
         if VERBOSE:
-            print(f'[INFO] PARAMS:', encoded_params)
+            print(f"[INFO] PARAMS:", encoded_params)
 
-        full_url = f'{url}?{encoded_params}'
+        full_url = f"{url}?{encoded_params}"
         with urllib.request.urlopen(full_url) as response:
-            data = response.read().decode('utf-8')
+            data = response.read().decode("utf-8")
             response_dict = json.loads(data)
 
-            if response_dict['result'] == 'success':
-                print('[INFO] Post-processing started successfully:', response_dict['message'])
+            if response_dict["result"] == "success":
+                print(
+                    "[INFO] Post-processing started successfully:",
+                    response_dict["message"],
+                )
                 return SUCCESS
 
-            print('[ERROR] Couldn\'t start Post-processing:', response_dict['message'])
+            print("[ERROR] Couldn't start Post-processing:", response_dict["message"])
             return ERROR
 
     except JSONDecodeError as ex:
-        print('[ERROR] Wrong API Key?')
+        print("[ERROR] Wrong API Key?")
 
     except Exception as ex:
-        print('[ERROR] Unexpected exception:', ex)
+        print("[ERROR] Unexpected exception:", ex)
         return ERROR
 
 
@@ -149,14 +158,14 @@ if COMMAND:
     sys.exit(ping_sickchill(URL))
 
 
-PATH = os.environ.get('NZBPP_FINALDIR') or os.environ['NZBPP_DIRECTORY']
-PROCESS_METHOD = METHODS_MAP[os.environ['NZBPO_PROCESSMETHOD']]
-FORCE_REPLACE = int(os.environ['NZBPO_FORCEREPLACE'] == 'yes')
-IS_PRIORITY = int(os.environ['NZBPO_ISPRIORITY'] == 'yes')
+PATH = os.environ.get("NZBPP_FINALDIR") or os.environ["NZBPP_DIRECTORY"]
+PROCESS_METHOD = METHODS_MAP[os.environ["NZBPO_PROCESSMETHOD"]]
+FORCE_REPLACE = int(os.environ["NZBPO_FORCEREPLACE"] == "yes")
+IS_PRIORITY = int(os.environ["NZBPO_ISPRIORITY"] == "yes")
 
 
 if VERBOSE:
-    print('[INFO] PATH:', PATH)
+    print("[INFO] PATH:", PATH)
 
 
 sys.exit(
--- tests.py
+++ tests.py
@@ -33,79 +33,81 @@ NONE = 95
 ERROR = 94
 
 ROOT_DIR = dirname(__file__)
-HOST = '127.0.0.1'
-PORT = '6789'
+HOST = "127.0.0.1"
+PORT = "6789"
 
 
 class HttpServerPingMock(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        data = {'data': {'pid': 5124}, 'message': 'Pong', 'result': 'success'}
+        data = {"data": {"pid": 5124}, "message": "Pong", "result": "success"}
         response = json.dumps(data)
-        self.wfile.write(response.encode('utf-8'))
+        self.wfile.write(response.encode("utf-8"))
 
 
 class HttpServerPostprocMock(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_url = urlparse(self.path)
         query_params = parse_qs(parsed_url.query)
-        cmd = query_params.get('cmd', [''])[0] == 'postprocess'
-        path = query_params.get('path', [''])[0] == ROOT_DIR
-        process_method = query_params.get('process_method', [''])[0] == 'move'
-        force_replace = query_params.get('force_replace', [''])[0] == '1'
-        is_priority = query_params.get('is_priority', [''])[0] == '1'
+        cmd = query_params.get("cmd", [""])[0] == "postprocess"
+        path = query_params.get("path", [""])[0] == ROOT_DIR
+        process_method = query_params.get("process_method", [""])[0] == "move"
+        force_replace = query_params.get("force_replace", [""])[0] == "1"
+        is_priority = query_params.get("is_priority", [""])[0] == "1"
 
-        if (cmd and path and process_method and force_replace and is_priority):
+        if cmd and path and process_method and force_replace and is_priority:
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {'data': {}, 'message': 'Started', 'result': 'success'}
+            data = {"data": {}, "message": "Started", "result": "success"}
             response = json.dumps(data)
-            self.wfile.write(response.encode('utf-8'))
+            self.wfile.write(response.encode("utf-8"))
         else:
             self.send_response(400)
-            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {'data': {}, 'message': 'Failure', 'result': 'failure'}
+            data = {"data": {}, "message": "Failure", "result": "failure"}
             response = json.dumps(data)
-            self.wfile.write(response.encode('utf-8'))
+            self.wfile.write(response.encode("utf-8"))
 
 
 def get_python():
-    if os.name == 'nt':
-        return 'python'
-    return 'python3'
+    if os.name == "nt":
+        return "python"
+    return "python3"
 
 
 def run_script():
     sys.stdout.flush()
-    proc = subprocess.Popen([get_python(), ROOT_DIR + '/main.py'],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            env=os.environ.copy())
+    proc = subprocess.Popen(
+        [get_python(), ROOT_DIR + "/main.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=os.environ.copy(),
+    )
     out, err = proc.communicate()
     ret_code = proc.returncode
     return (out.decode(), int(ret_code), err.decode())
 
 
 def set_default_env():
-    os.environ['NZBPO_APIKEY'] = 'API_KEY'
-    os.environ['NZBPP_DIRECTORY'] = ROOT_DIR
-    os.environ['NZBPO_HOST'] = HOST
-    os.environ['NZBPO_PORT'] = PORT
-    os.environ['NZBPO_PROCESSMETHOD'] = 'Copy'
-    os.environ['NZBPO_FORCEREPLACE'] = 'yes'
-    os.environ['NZBPO_ISPRIORITY'] = 'yes'
-    os.environ['NZBPO_VERBOSE'] = 'yes'
+    os.environ["NZBPO_APIKEY"] = "API_KEY"
+    os.environ["NZBPP_DIRECTORY"] = ROOT_DIR
+    os.environ["NZBPO_HOST"] = HOST
+    os.environ["NZBPO_PORT"] = PORT
+    os.environ["NZBPO_PROCESSMETHOD"] = "Copy"
+    os.environ["NZBPO_FORCEREPLACE"] = "yes"
+    os.environ["NZBPO_ISPRIORITY"] = "yes"
+    os.environ["NZBPO_VERBOSE"] = "yes"
 
 
 class Tests(unittest.TestCase):
     def test_command(self):
         set_default_env()
-        os.environ['NZBCP_COMMAND'] = 'ping'
+        os.environ["NZBCP_COMMAND"] = "ping"
         server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPingMock)
         thread = threading.Thread(target=server.serve_forever)
         thread.start()
@@ -117,7 +119,7 @@ class Tests(unittest.TestCase):
 
     def test_postproc(self):
         set_default_env()
-        os.environ['NZBPO_PROCESSMETHOD'] = 'Move'
+        os.environ["NZBPO_PROCESSMETHOD"] = "Move"
         server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
         thread = threading.Thread(target=server.serve_forever)
         thread.start()
@@ -129,7 +131,7 @@ class Tests(unittest.TestCase):
 
     def test_unsupported_method(self):
         set_default_env()
-        os.environ['NZBPO_PROCESSMETHOD'] = 'Unsupported method'
+        os.environ["NZBPO_PROCESSMETHOD"] = "Unsupported method"
         server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
         thread = threading.Thread(target=server.serve_forever)
         thread.start()
@@ -141,8 +143,8 @@ class Tests(unittest.TestCase):
 
     def test_no_path_provided(self):
         set_default_env()
-        os.environ.pop('NZBPP_DIRECTORY', None)
-        os.environ.pop('NZBCP_COMMAND', None)
+        os.environ.pop("NZBPP_DIRECTORY", None)
+        os.environ.pop("NZBCP_COMMAND", None)
         server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
         thread = threading.Thread(target=server.serve_forever)
         thread.start()
@@ -154,11 +156,11 @@ class Tests(unittest.TestCase):
 
     def test_use_final_dir(self):
         set_default_env()
-        DIR_PATH = 'D:\\downloads'
-        os.environ['NZBPP_DIRECTORY'] = DIR_PATH
-        os.environ['NZBPP_FINALDIR'] = ROOT_DIR
-        os.environ['NZBPO_PROCESSMETHOD'] = 'Move'
-        os.environ.pop('NZBCP_COMMAND', None)
+        DIR_PATH = "D:\\downloads"
+        os.environ["NZBPP_DIRECTORY"] = DIR_PATH
+        os.environ["NZBPP_FINALDIR"] = ROOT_DIR
+        os.environ["NZBPO_PROCESSMETHOD"] = "Move"
+        os.environ.pop("NZBCP_COMMAND", None)
         server = http.server.HTTPServer((HOST, int(PORT)), HttpServerPostprocMock)
         thread = threading.Thread(target=server.serve_forever)
         thread.start()
@@ -171,12 +173,12 @@ class Tests(unittest.TestCase):
         self.assertEqual(code, SUCCESS)
 
     def test_manifest(self):
-        with open(ROOT_DIR + '/manifest.json', encoding='utf-8') as file:
+        with open(ROOT_DIR + "/manifest.json", encoding="utf-8") as file:
             try:
                 json.loads(file.read())
             except ValueError as e:
-                self.fail('manifest.json is not valid.')
+                self.fail("manifest.json is not valid.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
